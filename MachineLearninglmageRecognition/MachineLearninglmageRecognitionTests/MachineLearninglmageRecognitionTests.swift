758d698bc8bbc39cfc79a5992b63a2ef8dabf352
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-02-04 14:21:54 +0300

    Add files via upload

---
+++ MachineLearninglmageRecognition/MachineLearninglmageRecognitionTests/MachineLearninglmageRecognitionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearninglmageRecognitionTests.swift
+//  MachineLearninglmageRecognitionTests
+//
+//  Created by Berkan Aydın on 17.01.2025.
+//
+
+import Testing
+@testable import MachineLearninglmageRecognition
+
+struct MachineLearninglmageRecognitionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
